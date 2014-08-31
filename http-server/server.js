723f4500a20ac78b5b66e1a5b3d3dfb7badc7674
Hektor Wallin <hektorw@gmail.com>
2014-08-31 23:45:07 +0200

    working http-websocket-server

--- http-server/server.js
+++ http-server/server.js
@@ -1,18 +1,93 @@
 
 var _ = require('underscore');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
 
 var app = require('express')();
-var bodyParser = require('body-parser');
 var server = require('http').Server(app);
 
-// app.use()
-app.get('/', function(req, res) {
-  console.log(req.url);
+
+var dirTemplate = _.template((function() {/*
+<!doctype html>
+  <head>
+    <title><%= name %></title>
+    <meta name="viewport" content="width=device-width">
+  </head>
+
+  <body>
+    <h1><%= url %></h1>
+    <ul>
+      <% _.each(files, function(file) { %>
+        <li><a href="<%- file.url %>"><%- file.name %></a></li>
+      <% }); %>
+    </ul>
+  </body>
+</html>
+*/}.toString().split('\n').slice(1, -1).join('\n')));
+
+
+app.get('*', function(req, res) {
+  var localpath = process.cwd() + req.url;
+  var url = req.url;
+
+  fs.stat(localpath, function(err, stat) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+      return;
+    }
+
+    if (stat.isFile()) {
+      // File
+
+      fs.readFile(localpath, function(err, data) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+          return;
+        }
+
+        var mimetype = mime.lookup(localpath);
+
+        console.log('mime ', mimetype, localpath);
+
+        res.type(mimetype);
+        res.send(data);
+      });
+
+    } else if (stat.isDirectory()) {
+      // Directory
+
+      fs.readdir(localpath, function(err, files) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+          return;
+        }
+
+        var html = dirTemplate({
+          name: path.basename(url),
+          url: url,
+          files: _.map(files, function(file) {
+            return {
+              url: path.join(url, file),
+              name: file
+            }
+          })
+        });
+
+        res.type('text/html');
+        res.send(html);
+      });
+    }
+  });
 });
 
 
 
 var port = 8080;
 server.listen(port);
+
 
 module.exports = server;
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-
+#! /usr/bin/env node
 
 var server = require('./http-server/server.js');
-// var io = require('socket.io')(server);
-var io = require('socket.io').listen(8090);
+var io = require('socket.io')(server);
+// var io = require('socket.io').listen(8090);
 var Router = require('./websocket-router/router.js');
 
 io.on('connection', function(socket) {
