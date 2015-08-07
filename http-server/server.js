02fd9184fce89d492771d2cb838646ec2b0f8d3e
Hektor Wallin <hektorw@gmail.com>
2015-08-07 10:02:50 +0200

    Updated server to using express-static

--- http-server/server.js
+++ http-server/server.js
@@ -1,126 +1,28 @@
+/// <reference path="../typings/node/node.d.ts"/>
 
-var colors = require('colors'),
-    errorcodes = require('./errorcodes.js'),
-    _ = require('underscore'),
-    fs = require('fs'),
-    path = require('path'),
-    mime = require('mime'),
-    app = require('express')(),
-    server = require('http').Server(app),
-    argv = require('minimist')(process.argv.slice(2));
+var colors = require('colors');
+var express = require('express');
+var minimist = require('minimist');
+var path = require('path');
+var http = require('http');
+var serveIndex = require('serve-index');
 
-var port = parseInt(argv.port, 10) || 8080;
+var args = minimist(process.argv.slice(2));
 
-var dirTemplate = _.template((function() {/*
-<!doctype html>
-  <head>
-    <title><%= name %></title>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <style>
-      body{color:#111;}
-      a{color:#0074D9;transition:color 0.2s;}
-      a:hover{color:#7FDBFF;}
-      a:visited{color:#85144B;}
-      li{color:#111;}
-    </style>
-  </head>
+var publicRoot = args._[0] || '';
+var serverRoot = path.join(process.cwd(), publicRoot);
 
-  <body>
-    <h1><%= url %></h1>
-    <ul>
-      <% _.each(files, function(file) { %>
-        <li><a href="<%- file.url %>"><%- file.name %></a></li>
-      <% }); %>
-    </ul>
-  </body>
-</html>
-*/}.toString().split('\n').slice(1, -1).join('\n')));
+var serverPort = parseInt(args.root, 10) || 8080; 
+
+var app = express();
+
+app.use(express.static(serverRoot));
+app.use(serveIndex(serverRoot, { icons: true }));
 
 
-
-
-
-
-app.get('*', function(req, res) {
-  var localpath = process.cwd() + req.url;
-  var url = req.url;
-
-  fs.stat(localpath, function(err, stat) {
-    if (err) {
-      sendError(res, err);
-      return;
-    }
-
-    if (stat.isFile()) {
-      // File
-      sendFile(res, localpath);
-
-    } else if (stat.isDirectory()) {
-
-      // Directory
-      sendDirectory(res, localpath, url);
-
-    } else {
-
-      // Send server error
-
-    }
-  });
+var server = http.Server(app);
+server.listen(serverPort, function() {
+	console.log(('Server listening at http://' + server.address().address + ':' + server.address().port).yellow);
 });
-
-function sendFile(response, localpath) {
-  fs.readFile(localpath, function(err, data) {
-    if (err) {
-      sendError(response, err);
-      return;
-    }
-
-    var mimetype = mime.lookup(localpath);
-
-    response.type(mimetype);
-    response.send(data);
-  });
-}
-
-function sendDirectory(response, localpath, url) {
-  fs.readdir(localpath, function(err, files) {
-    if (err) {
-      sendError(response, err);
-      return;
-    }
-
-    var indexFile = _.find(files, function(file) {
-      return file.toLowerCase() === 'index.html' || file.toLowerCase() === 'index.htm';
-    });
-    if (indexFile) {
-      sendFile(response, path.join(localpath, indexFile));
-    } else {
-      var html = dirTemplate({
-        name: path.basename(url),
-        url: url,
-        files: _.map(files, function(file) {
-          return {
-            url: path.join(url, file),
-            name: file
-          };
-        })
-      });
-
-      response.type('text/html');
-      response.send(html);
-    }
-
-  });
-}
-
-function sendError(response, error) {
-  var httpError = errorcodes.getError(error.errno);
-  response.status(httpError.code).send(httpError.description);
-}
-
-
-
-console.log(('started listening on port: ' + port).yellow);
-server.listen(port);
 
 module.exports = server;
